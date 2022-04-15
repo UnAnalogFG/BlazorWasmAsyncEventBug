8b218d5da5bdb349d7a6dfa8c47319be463f65c0
UnAnalog <mail@unanalog.com>
2022-04-15 12:50:39 +0200

    COre to show the behaviour of Async Events using await

---
+++ Pages/TestData.cs
@@ -0,0 +1,14 @@
+﻿namespace GLAdmin.Client.V6.Pages._Test;
+
+public class TestData
+{
+    public string Time = DateTime.UtcNow.ToLocalTime().ToLongTimeString();
+    public string Source{ get; set; }
+    public int Value { get; set; }
+
+    public TestData(string source, int v)
+    {
+        Source = source;
+        Value = v;
+    }
+}
